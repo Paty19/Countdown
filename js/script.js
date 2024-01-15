e3237121b7eb9538c68e2f365d0832c1e176f8fd
PatimatAzizova <112445417+Paty19@users.noreply.github.com>
2024-01-15 21:15:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ else{
 
 
 function christmasCountdown() {
-  const christmasDate = new Date("December 25, 2023 00:00");
+  const christmasDate = new Date("December 25, 2024 00:00");
   const now = new Date();
   const diff = christmasDate - now;
 
